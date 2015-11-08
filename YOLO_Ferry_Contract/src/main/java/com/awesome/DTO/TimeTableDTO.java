51cf1f410b097a2606953fdf1ef8aa124ddd35b9
Timea Kiss <k.timea.k@gmail.com>
2015-11-08 20:24:36 +0100

    TimeTableDTO is added to handle the departure times of the ferries in connection to the Harbors.
    The CustomerContract in it's present time format is accounting for 2 Use Cases
    - Select Trip
    - Booking (customer)

---
+++ YOLO_Ferry_Contract/src/main/java/com/awesome/DTO/TimeTableDTO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.awesome.DTO;
+
+import java.sql.Time;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Timea Kiss <cph-tk10@cphbusiness.dk>
+ */
+public class TimeTableDTO {
+    
+    private ArrayList<Time> sailingTimes;
+    
+}
--- YOLO_Ferry_Contract/src/main/java/com/awesome/yolo_ferry_contract/CustomerContract.java
+++ YOLO_Ferry_Contract/src/main/java/com/awesome/yolo_ferry_contract/CustomerContract.java
@@ -6,10 +6,22 @@
 
 package com.awesome.yolo_ferry_contract;
 
+import com.awesome.DTO.BookingDTO;
+import com.awesome.DTO.HarborDTO;
+import com.awesome.DTO.RouteDTO;
+import com.awesome.DTO.TimeTableDTO;
+import java.util.ArrayList;
+
 /**
  *
  * @author Timea Kiss <cph-tk10@cphbusiness.dk>
  */
-public class CustomerContract {
+public interface CustomerContract {
+    
+    public ArrayList<HarborDTO> getAllHarbors();
+    //list of possible routes based on the departure harbor
+    public ArrayList<RouteDTO> getHarborRoutes(int harborID);
+    public ArrayList<TimeTableDTO> getTimeTable(int routeID);
+    public boolean createBooking(BookingDTO booking);
     
 }
