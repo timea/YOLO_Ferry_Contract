565d28df5f2be2422f2a6547eec911955d8d4aad
Timea Kiss <k.timea.k@gmail.com>
2015-11-08 18:52:42 +0100

    Initial commit of the contract repo with the maven project and 2 classes for the interfaces/contracts

---
+++ YOLO_Ferry_Contract/src/main/java/com/awesome/yolo_ferry_contract/App.java
@@ -0,0 +1,13 @@
+package com.awesome.yolo_ferry_contract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
